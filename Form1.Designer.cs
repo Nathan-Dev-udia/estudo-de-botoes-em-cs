5320b03ba2ad15f555f8f81ea755de7bd52e2f8d
Nathan Fernandes <nathan.dev.udia@gmail.com>
2025-06-18 17:31:49 -0300

    Add files via upload

---
+++ Form1.Designer.cs
@@ -0,0 +1,163 @@
+﻿namespace butoes
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variável de designer necessária.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpar os recursos que estão sendo usados.
+        /// </summary>
+        /// <param name="disposing">true se for necessário descartar os recursos gerenciados; caso contrário, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código gerado pelo Windows Form Designer
+
+        /// <summary>
+        /// Método necessário para suporte ao Designer - não modifique 
+        /// o conteúdo deste método com o editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.button1 = new System.Windows.Forms.Button();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.vScrollBar1 = new System.Windows.Forms.VScrollBar();
+            this.nToggle1 = new butoes.Nbotoesp.NToggle();
+            this.panel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.FlatAppearance.BorderSize = 0;
+            this.button1.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button1.Font = new System.Drawing.Font("Century Gothic", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.Image = ((System.Drawing.Image)(resources.GetObject("button1.Image")));
+            this.button1.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.button1.Location = new System.Drawing.Point(3, 9);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(140, 45);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "Home";
+            this.button1.TextImageRelation = System.Windows.Forms.TextImageRelation.ImageBeforeText;
+            this.button1.UseVisualStyleBackColor = true;
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(128)))), ((int)(((byte)(255)))), ((int)(((byte)(128)))));
+            this.panel1.Controls.Add(this.button4);
+            this.panel1.Controls.Add(this.button3);
+            this.panel1.Controls.Add(this.button2);
+            this.panel1.Controls.Add(this.button1);
+            this.panel1.Location = new System.Drawing.Point(0, -5);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(146, 320);
+            this.panel1.TabIndex = 3;
+            // 
+            // button2
+            // 
+            this.button2.FlatAppearance.BorderSize = 0;
+            this.button2.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button2.Font = new System.Drawing.Font("Century Gothic", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button2.Image = ((System.Drawing.Image)(resources.GetObject("button2.Image")));
+            this.button2.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.button2.Location = new System.Drawing.Point(3, 60);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(140, 45);
+            this.button2.TabIndex = 4;
+            this.button2.Text = "Importação";
+            this.button2.TextImageRelation = System.Windows.Forms.TextImageRelation.ImageBeforeText;
+            this.button2.UseVisualStyleBackColor = true;
+            // 
+            // button3
+            // 
+            this.button3.FlatAppearance.BorderSize = 0;
+            this.button3.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button3.Font = new System.Drawing.Font("Century Gothic", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button3.Image = ((System.Drawing.Image)(resources.GetObject("button3.Image")));
+            this.button3.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.button3.Location = new System.Drawing.Point(3, 111);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(140, 45);
+            this.button3.TabIndex = 5;
+            this.button3.Text = "Não sei";
+            this.button3.TextImageRelation = System.Windows.Forms.TextImageRelation.ImageBeforeText;
+            this.button3.UseVisualStyleBackColor = true;
+            // 
+            // button4
+            // 
+            this.button4.FlatAppearance.BorderSize = 0;
+            this.button4.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button4.Font = new System.Drawing.Font("Century Gothic", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button4.Image = ((System.Drawing.Image)(resources.GetObject("button4.Image")));
+            this.button4.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.button4.Location = new System.Drawing.Point(3, 162);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(140, 45);
+            this.button4.TabIndex = 6;
+            this.button4.Text = "Config";
+            this.button4.TextImageRelation = System.Windows.Forms.TextImageRelation.ImageBeforeText;
+            this.button4.UseVisualStyleBackColor = true;
+            // 
+            // vScrollBar1
+            // 
+            this.vScrollBar1.Location = new System.Drawing.Point(683, -5);
+            this.vScrollBar1.Name = "vScrollBar1";
+            this.vScrollBar1.Size = new System.Drawing.Size(17, 320);
+            this.vScrollBar1.TabIndex = 4;
+            // 
+            // nToggle1
+            // 
+            this.nToggle1.BackColor = System.Drawing.SystemColors.Control;
+            this.nToggle1.Checked = true;
+            this.nToggle1.CheckState = System.Windows.Forms.CheckState.Checked;
+            this.nToggle1.Location = new System.Drawing.Point(602, 12);
+            this.nToggle1.MinimumSize = new System.Drawing.Size(45, 22);
+            this.nToggle1.Name = "nToggle1";
+            this.nToggle1.OffBackColor = System.Drawing.Color.Gray;
+            this.nToggle1.OffToggleColor = System.Drawing.Color.Red;
+            this.nToggle1.OnBackColor = System.Drawing.Color.Lime;
+            this.nToggle1.OnToggleColor = System.Drawing.Color.WhiteSmoke;
+            this.nToggle1.Size = new System.Drawing.Size(62, 29);
+            this.nToggle1.TabIndex = 0;
+            this.nToggle1.UseVisualStyleBackColor = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.vScrollBar1);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.nToggle1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.panel1.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private Nbotoesp.NToggle nToggle1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.VScrollBar vScrollBar1;
+    }
+}
+
---
+++ Nbotoesp/NButton.cs
@@ -0,0 +1,128 @@
+﻿using System;
+using System.Drawing;
+using System.Drawing.Drawing2D;
+using System.Windows.Forms;
+
+namespace butoes.Nbotoesp
+{
+    class NButton : Button
+    {
+        private int borderSize = 3; // Tamanho da borda
+        private int borderRadius = 20; // Raio da curvatura do botão
+        private Color borderColor = Color.MediumSlateBlue; // Cor da borda do botão
+        private Color originalBackColor; // Cor de fundo original do botão
+        private Color clickedBackColor = Color.LightSlateGray; // Cor do fundo quando o botão for clicado
+        private Color clickedBorderColor = Color.Cyan; // Cor da borda quando o botão for clicado
+        private Color clickedTextColor = Color.Black; // Cor do texto quando o botão for clicado
+
+        // Propriedades públicas para edição via Designer
+        public Color BorderColor
+        {
+            get { return borderColor; }
+            set { borderColor = value; Invalidate(); }
+        }
+
+        public Color ClickedBackColor
+        {
+            get { return clickedBackColor; }
+            set { clickedBackColor = value; Invalidate(); }
+        }
+
+        public Color ClickedBorderColor
+        {
+            get { return clickedBorderColor; }
+            set { clickedBorderColor = value; Invalidate(); }
+        }
+
+        public Color ClickedTextColor
+        {
+            get { return clickedTextColor; }
+            set { clickedTextColor = value; Invalidate(); }
+        }
+
+        public NButton()
+        {
+            this.FlatStyle = FlatStyle.Flat;
+            this.FlatAppearance.BorderSize = 0;
+            this.BackColor = Color.MediumSlateBlue; // Cor de fundo do botão
+            this.ForeColor = Color.White; // Cor do texto do botão
+            this.originalBackColor = this.BackColor; // Armazenar a cor original
+
+            this.Resize += (s, e) => this.Invalidate();
+            this.MouseDown += (s, e) => this.OnMouseDown(e); // Evento para detectar quando o botão for pressionado
+            this.MouseUp += (s, e) => this.OnMouseUp(e); // Evento para detectar quando o botão for solto
+        }
+
+        // Evento quando o botão for pressionado (fica pálido)
+        private void OnMouseDown(MouseEventArgs e)
+        {
+            this.BackColor = clickedBackColor; // Altera a cor de fundo para o clicado
+            this.borderColor = clickedBorderColor; // Altera a cor da borda para o clicado
+            this.ForeColor = clickedTextColor; // Altera a cor do texto para o clicado
+            this.Invalidate(); // Força o botão a ser repintado com a nova cor
+        }
+
+        // Evento quando o botão for solto (volta para a cor original)
+        private void OnMouseUp(MouseEventArgs e)
+        {
+            this.BackColor = originalBackColor; // Restaura a cor original
+            this.borderColor = Color.MediumSlateBlue; // Restaura a cor original da borda
+            this.ForeColor = Color.White; // Restaura a cor original do texto
+        }
+
+        protected override void OnPaint(PaintEventArgs pevent)
+        {
+            base.OnPaint(pevent);
+            pevent.Graphics.SmoothingMode = SmoothingMode.AntiAlias;
+
+            int radius = Math.Min(borderRadius, Math.Min(this.Width, this.Height) / 2); // Evita deformações na curvatura
+            Rectangle rectSurface = new Rectangle(0, 0, this.Width, this.Height);
+            Rectangle rectBorder = new Rectangle(1, 1, this.Width - 2, this.Height - 2);
+
+            // Desenhar o botão com bordas arredondadas
+            using (GraphicsPath pathSurface = GetFigurePath(rectSurface, radius))
+            using (GraphicsPath pathBorder = GetFigurePath(rectBorder, radius - 1))
+            using (Pen penBorder = new Pen(borderColor, borderSize))
+            {
+                penBorder.Alignment = PenAlignment.Inset;
+                this.Region = new Region(pathSurface); // Aplica a curvatura ao botão
+
+                pevent.Graphics.FillPath(new SolidBrush(this.BackColor), pathSurface); // Preenche o botão com a cor de fundo
+                pevent.Graphics.DrawPath(penBorder, pathBorder); // Desenha a borda do botão
+            }
+
+            // Desenhar o texto no botão
+            using (Brush textBrush = new SolidBrush(this.ForeColor))
+            {
+                StringFormat sf = new StringFormat
+                {
+                    Alignment = StringAlignment.Center,
+                    LineAlignment = StringAlignment.Center
+                };
+                pevent.Graphics.DrawString(this.Text, this.Font, textBrush, rectSurface, sf); // Desenha o texto centralizado
+            }
+        }
+
+        private GraphicsPath GetFigurePath(Rectangle rect, int radius)
+        {
+            GraphicsPath path = new GraphicsPath();
+            int curveSize = radius * 2;
+
+            path.StartFigure();
+            path.AddArc(rect.X, rect.Y, curveSize, curveSize, 180, 90);
+            path.AddArc(rect.Right - curveSize, rect.Y, curveSize, curveSize, 270, 90);
+            path.AddArc(rect.Right - curveSize, rect.Bottom - curveSize, curveSize, curveSize, 0, 90);
+            path.AddArc(rect.X, rect.Bottom - curveSize, curveSize, curveSize, 90, 90);
+            path.CloseFigure();
+
+            return path;
+        }
+
+        protected override void OnHandleCreated(EventArgs e)
+        {
+            base.OnHandleCreated(e);
+            if (this.Parent != null)
+                this.Parent.BackColorChanged += (s, ev) => this.Invalidate();
+        }
+    }
+}
